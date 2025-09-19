319d7d9a874bc86f8b378c8c6ac46578eb0952b9
Cui Chen <i@cuichen.cc>
2025-09-19 08:27:57 +0800

    Add template-haskell dependency and enable Template Haskell language extension in Main.hs
    Update package.yaml and type-inference-zoo.cabal to include template-haskell as a dependency. Modify Main.hs to enable the Template Haskell language extension for improved file handling capabilities.

--- app/Main.hs
+++ app/Main.hs
@@ -1,15 +1,17 @@
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE TemplateHaskell #-}
 
 module Main (main) where
 
-import Alg (runAlgInferVariant, runAlgSubVariant, getAllAlgMeta)
+import Alg (getAllAlgMeta, runAlgInferVariant, runAlgSubVariant)
 import Data.Foldable (find)
-import Lib (InferResult (..), inferResultToJson, algMetasToJson)
+import Language.Haskell.TH (runIO)
+import Language.Haskell.TH.Syntax (qAddDependentFile)
+import Lib (InferResult (..), algMetasToJson, inferResultToJson)
 import Opt (Option (..), options)
 import Parser (parseTrm, parseTyp)
 import System.Console.GetOpt (ArgOrder (Permute), getOpt)
 import System.Environment (getArgs)
-import System.IO (readFile)
 
 main :: IO ()
 main = do
@@ -17,22 +19,20 @@ main = do
   case getOpt Permute options args of
     (flags, [], [])
       | Meta `elem` flags -> putStrLn $ algMetasToJson getAllAlgMeta
-      | Grammar `elem` flags -> do
-          content <- readFile "lc.tmLanguage"
-          putStr content
+      | Grammar `elem` flags -> putStr $(qAddDependentFile "lc.tmLanguage" >> runIO (readFile "lc.tmLanguage") >>= \content -> [|content|])
     (flags, [code], [])
       | Just (Typing algName) <- find (\case Typing _ -> True; _ -> False) flags -> do
           let variant = case find (\case Variant _ -> True; _ -> False) flags of
-                         Just (Variant v) -> Just v
-                         _ -> Nothing
+                Just (Variant v) -> Just v
+                _ -> Nothing
           case parseTrm code of
             Left err -> putStrLn $ inferResultToJson $ InferResult False Nothing [] (Just err) False
             Right tm -> putStrLn $ inferResultToJson $ runAlgInferVariant algName variant tm
     (flags, [source, target], [])
       | Just (Subtyping algName) <- find (\case Subtyping _ -> True; _ -> False) flags -> do
           let variant = case find (\case Variant _ -> True; _ -> False) flags of
-                         Just (Variant v) -> Just v
-                         _ -> Nothing
+                Just (Variant v) -> Just v
+                _ -> Nothing
           case (parseTyp source, parseTyp target) of
             (Left err, _) -> putStrLn $ inferResultToJson $ InferResult False Nothing [] (Just err) False
             (_, Left err) -> putStrLn $ inferResultToJson $ InferResult False Nothing [] (Just err) False
